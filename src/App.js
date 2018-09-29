0b909884ce9387fd7726ef1675fb8bfae22b63c0
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-09-29 13:15:17 +0530

    app.js added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import TodoInput from "./ToDoInput";
+import ToDoList from "./ToDoList";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://sela.herokuapp.com/v1alpha1/graphql" }),
+  cache: new InMemoryCache()
+});
+
+var user_id = "1";
+var todo_index = 1;
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="todoForm">
+      <h3 className="title">To Do List</h3>
+      <TodoInput user_id={user_id} todo_index={todo_index} />
+      <ToDoList user_id={user_id} />
+    </div>
+  </ApolloProvider>
+);
+
+export default App;
