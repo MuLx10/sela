e79886f5a92f35e415fc071b8d1cb58f2dc20875
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-09-29 18:28:06 +0530

    added gql auth"

--- src/Sela.js
+++ src/Sela.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { setContext } from 'apollo-link-context'
 import TodoInput from "./ToDo/ToDoInput";
 import ToDoList from "./ToDo/ToDoList";
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  console.log(token);
+  return {
+    headers: {
+      ...headers,
+      'X-Hasura-Access-Key':token
+    }
+  }
+})
+
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "https://sela.herokuapp.com/v1alpha1/graphql" }),
-  cache: new InMemoryCache()
+  link: authLink.concat(new HttpLink({ uri: 'https://selagql.herokuapp.com/v1alpha1/graphql' })),
+  cache: new InMemoryCache(),
 });
 
 const Sela = ({ user_id, todo_index }) => (
