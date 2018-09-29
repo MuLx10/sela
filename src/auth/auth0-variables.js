8ae87a0e61c9cfea2cbcfb75ced709357d4290e1
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-09-29 18:54:16 +0530

    callback

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'sela.auth0.com',
   clientId: 'SgAbnEU9YmNnPZhdVZC3EI99GKBGa73E',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://sela.herokuapp.com/callback'
 }
