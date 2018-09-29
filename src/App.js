56b1b4fe118ae04a9b459a553bc30ae6c3ab26b7
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-09-29 13:34:01 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import TodoInput from "./ToDoInput";
